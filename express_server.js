77f6142b757d4abeb5849df9cd753fb230c7b37c
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-06-04 00:12:45 -0400

    implement redirect to longUrl when short Url is clicked

--- express_server.js
+++ express_server.js
@@ -36,12 +36,18 @@ app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   const id  = generateRandomString();
   urlDatabase[id] = req.body.longURL;
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${id}`)
+  //res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("pages/urls_show", templateVars);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.get("/urls.json", (req, res) => {
