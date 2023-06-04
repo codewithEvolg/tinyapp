c15cf9ed6deeb924bd12cab2627ba893ee909875
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-06-03 23:43:19 -0400

    added a post route to handle newly submitted urls

--- express_server.js
+++ express_server.js
@@ -7,11 +7,22 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//define middlewares here
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("pages/urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("pages/urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
